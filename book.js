f4a3635223741feee23404cad89fc7f600b7f4a5
PJ <96671928+Peaksenut@users.noreply.github.com>
2023-05-18 21:29:53 +0800

    improve readability

--- book.js
+++ book.js
@@ -1,17 +1,16 @@
 let myLibrary = [];
 
 const firstBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
-addBookToLibrary(firstBook, myLibrary);
 
-const submitBtn = document.querySelector('input[type ="submit"]');
+function Book(title, author, noPages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.noPages = noPages;
+  this.isRead = isRead ? "finished" : "unfinished";
+}
+
+
 const cardContainer = document.querySelector('container');
-const modalContainer = document.querySelector('.modal-container');
-const trigger = document.querySelector('#add-book');
-const closeBtn = document.querySelector('.close-button');
-const pagesField = document.getElementById('pages');
-
-submitBtn.addEventListener('click', submit);
-
 cardContainer.addEventListener('click', function (e) {
   if (e.target.classList.contains('isread-btn')) {
     updateReadState.call(e.target);
@@ -23,71 +22,10 @@ cardContainer.addEventListener('click', function (e) {
   } else if (e.target.parentNode.classList.contains('remove-btn')) {
     deleteBook.call(e.target.parentNode);
   }
-})
-
-trigger.addEventListener('click', toggleModal);
-closeBtn.addEventListener('click', toggleModal);
-window.addEventListener('click', windowOnCLick);
-pagesField.addEventListener('keypress', checkPages);
-
-function submit(e) {
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
-  const pagesInput = document.getElementById('pages');
-
-  const title = titleInput.value.trim();
-  const author = authorInput.value.trim();
-  const pages = parseInt(pagesInput.value);
-
-  if (title === '' || author === '' || isNaN(pages)) {
-    const warningMsg = document.querySelector('.required');
-    warningMsg.style.display = 'block';
-
-    setTimeout(function () {
-      warningMsg.style.display = 'none';
-    }, 2000);
-  } else if (myLibrary.some(book => book.title.toLowerCase() === title.toLowerCase() && book.author.toLowerCase() === author.toLowerCase())) {
-    const existsWarningMsg = document.querySelector('.exists');
-    existsWarningMsg.style.display = 'block';
-
-    setTimeout(function () {
-      existsWarningMsg.style.display = 'none';
-    }, 2000);
-  } else {
-    toggleModal();
-    const newBook = new Book(title, author, pages, false);
-    addBookToLibrary(newBook, myLibrary);
-  }
-
-  e.preventDefault();
-}
-
-function clearFields() {
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
-  const pagesInput = document.getElementById('pages');
-
-  titleInput.value = '';
-  authorInput.value = '';
-  pagesInput.value = ''
-}
-
-function Book(title, author, noPages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.noPages = noPages;
-  this.isRead = isRead ? "finished" : "unfinished";
-}
-
-function addBookToLibrary(Book, lib) {
-  lib.push(Book);
-  displayBook(Book);
-}
+});
 
 
 function displayBook(book) {
-  const cardContainer = document.querySelector('container');
-
   const newCard = document.createElement('div');
   newCard.className = 'card';
 
@@ -132,6 +70,15 @@ function displayBook(book) {
   addBorderifRead.call(isReadBtn);
 }
 
+
+addBookToLibrary(firstBook, myLibrary);
+
+function addBookToLibrary(Book, lib) {
+  lib.push(Book);
+  displayBook(Book);
+}
+
+
 function updateReadState() {
   if (this.textContent == 'finished') {
     this.textContent = 'unfinished';
@@ -139,6 +86,7 @@ function updateReadState() {
     this.textContent = 'finished';
   }
 }
+
 
 function addBorderifRead() {
   const parent = this.parentNode;
@@ -150,6 +98,7 @@ function addBorderifRead() {
     grandparent.classList.remove('finished-card');
   }
 }
+
 
 function deleteBook() {
   const parent = this.parentNode;
@@ -163,16 +112,29 @@ function deleteBook() {
   grandparent.remove();
 }
 
+
+const modalContainer = document.querySelector('.modal-container');
+const trigger = document.querySelector('#add-book');
+const closeBtn = document.querySelector('.close-button');
+const pagesField = document.getElementById('pages');
+
+trigger.addEventListener('click', toggleModal);
+closeBtn.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnCLick);
+pagesField.addEventListener('keypress', checkPages);
+
 function toggleModal() {
   modalContainer.classList.toggle("show");
   clearFields();
 }
+
 
 function windowOnCLick(e) {
   if (e.target === modalContainer) {
     toggleModal();
   }
 }
+
 
 function checkPages(e) {
   const charCode = e.charCode;
@@ -182,4 +144,51 @@ function checkPages(e) {
 
   e.preventDefault();
   return false;
+}
+
+
+const submitBtn = document.querySelector('input[type ="submit"]');
+submitBtn.addEventListener('click', submit);
+
+function submit(e) {
+  const titleInput = document.getElementById('title');
+  const authorInput = document.getElementById('author');
+  const pagesInput = document.getElementById('pages');
+
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
+  const pages = parseInt(pagesInput.value);
+
+  if (title === '' || author === '' || isNaN(pages)) {
+    const warningMsg = document.querySelector('.required');
+    warningMsg.style.display = 'block';
+
+    setTimeout(function () {
+      warningMsg.style.display = 'none';
+    }, 2000);
+  } else if (myLibrary.some(book => book.title.toLowerCase() === title.toLowerCase() && book.author.toLowerCase() === author.toLowerCase())) {
+    const existsWarningMsg = document.querySelector('.exists');
+    existsWarningMsg.style.display = 'block';
+
+    setTimeout(function () {
+      existsWarningMsg.style.display = 'none';
+    }, 2000);
+  } else {
+    toggleModal();
+    const newBook = new Book(title, author, pages, false);
+    addBookToLibrary(newBook, myLibrary);
+  }
+
+  e.preventDefault();
+}
+
+
+function clearFields() {
+  const titleInput = document.getElementById('title');
+  const authorInput = document.getElementById('author');
+  const pagesInput = document.getElementById('pages');
+
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = ''
 }
