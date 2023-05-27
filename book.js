676cb20eaad922411e353597e43233cdac614d50
PJ <96671928+Peaksenut@users.noreply.github.com>
2023-05-27 17:49:48 +0800

    refactor code to use class

--- book.js
+++ book.js
@@ -1,140 +1,136 @@
-let myLibrary = [];
+class Library {
+  constructor() {
+    this.bookshelf = [];
+  }
 
-const firstBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+  cardContainer = document.querySelector('container');
 
-function Book(title, author, noPages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.noPages = noPages;
-  this.isRead = isRead ? "finished" : "unfinished";
+  add(book) {
+    this.bookshelf.push(book);
+    this.display(book);
+  }
+
+  display(book) {
+    const newCard = document.createElement('div');
+    newCard.className = 'card';
+
+    const title = document.createElement('h2');
+    title.textContent = book.title;
+    title.className = 'title';
+
+    const author = document.createElement('p');
+    author.textContent = book.author;
+    author.className = 'author';
+
+    const pages = document.createElement('p');
+    pages.textContent = `${book.pages} pages`;
+    pages.className = 'pages';
+
+    const btnContainer = document.createElement('div');
+    btnContainer.className = 'btn-container';
+
+    const isReadBtn = document.createElement('button');
+    isReadBtn.textContent = book.isRead ? "finished" : "unfinished";
+    isReadBtn.className = 'isread-btn';
+
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'remove-btn';
+    const icon = document.createElement('img');
+    icon.className = 'remove-icon';
+    icon.src = 'delete.png';
+    icon.alt = 'Delete';
+
+    removeBtn.setAttribute('data-index', `${book.title}-${book.author}`);
+
+    newCard.appendChild(title);
+    newCard.appendChild(author);
+    newCard.appendChild(pages);
+    newCard.appendChild(isReadBtn);
+    newCard.appendChild(btnContainer);
+    btnContainer.appendChild(isReadBtn);
+    btnContainer.appendChild(removeBtn);
+    removeBtn.appendChild(icon);
+
+    this.cardContainer.appendChild(newCard);
+
+    this.addBorder.call(isReadBtn);
+  }
+
+  updateIsRead() {
+    if (this.textContent == 'finished') {
+      this.textContent = 'unfinished';
+    } else {
+      this.textContent = 'finished';
+    }
+  }
+
+  addBorder() {
+    const parent = this.parentNode;
+    const grandparent = parent.parentNode;
+
+    if (this.textContent == 'finished') {
+      grandparent.classList.add('finished-card');
+    } else {
+      grandparent.classList.remove('finished-card');
+    }
+  }
+
+  delete(target) {
+    const parent = target.parentNode;
+    const grandparent = parent.parentNode;
+
+    this.bookshelf = this.bookshelf.filter(book => {
+      return `${book.title}-${book.author}` !== target.getAttribute('data-index');
+    });
+
+    grandparent.remove();
+  }
 }
 
+function createBook(title, author, pages, isRead) {
+  return { title, author, pages, isRead };
+}
 
+const myLibrary = new Library();
+const firstBook = createBook('The Hobbit', 'J.R.R. Tolkien', 295, true);
+myLibrary.add(firstBook);
+
+//event listeners
 const cardContainer = document.querySelector('container');
-cardContainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('isread-btn')) {
-    updateReadState.call(e.target);
-    addBorderifRead.call(e.target);
-  }
-
-  if (e.target.classList.contains('remove-btn')) {
-    deleteBook.call(e.target);
-  } else if (e.target.parentNode.classList.contains('remove-btn')) {
-    deleteBook.call(e.target.parentNode);
-  }
-});
-
-
-function displayBook(book) {
-  const newCard = document.createElement('div');
-  newCard.className = 'card';
-
-  const title = document.createElement('h2');
-  title.textContent = book.title;
-  title.className = 'title';
-
-  const author = document.createElement('p');
-  author.textContent = book.author;
-  author.className = 'author';
-
-  const pages = document.createElement('p');
-  pages.textContent = `${book.noPages} pages`;
-  pages.className = 'pages';
-
-  const btnContainer = document.createElement('div');
-  btnContainer.className = 'btn-container';
-
-  const isReadBtn = document.createElement('button');
-  isReadBtn.textContent = book.isRead;
-  isReadBtn.className = 'isread-btn';
-
-  const removeBtn = document.createElement('button');
-  removeBtn.className = 'remove-btn';
-  const icon = document.createElement('img');
-  icon.className = 'remove-icon';
-  icon.src = 'delete.png';
-  icon.alt = 'Delete';
-
-  removeBtn.setAttribute('data-index', `${book.title}-${book.author}`);
-
-  newCard.appendChild(title);
-  newCard.appendChild(author);
-  newCard.appendChild(pages);
-  newCard.appendChild(isReadBtn);
-  newCard.appendChild(btnContainer);
-  btnContainer.appendChild(isReadBtn);
-  btnContainer.appendChild(removeBtn);
-  removeBtn.appendChild(icon);
-
-  cardContainer.appendChild(newCard);
-  addBorderifRead.call(isReadBtn);
-}
-
-
-addBookToLibrary(firstBook, myLibrary);
-
-function addBookToLibrary(Book, lib) {
-  lib.push(Book);
-  displayBook(Book);
-}
-
-
-function updateReadState() {
-  if (this.textContent == 'finished') {
-    this.textContent = 'unfinished';
-  } else {
-    this.textContent = 'finished';
-  }
-}
-
-
-function addBorderifRead() {
-  const parent = this.parentNode;
-  const grandparent = parent.parentNode;
-
-  if (this.textContent == 'finished') {
-    grandparent.classList.add('finished-card');
-  } else {
-    grandparent.classList.remove('finished-card');
-  }
-}
-
-
-function deleteBook() {
-  const parent = this.parentNode;
-  const grandparent = parent.parentNode;
-  const removeElement = this.getAttribute('data-index')
-
-  myLibrary = myLibrary.filter(book => {
-    const bookToRemove = `${book.title}-${book.author}`;
-    return bookToRemove !== removeElement;
-  });
-  grandparent.remove();
-}
-
-
 const modalContainer = document.querySelector('.modal-container');
 const trigger = document.querySelector('#add-book');
 const closeBtn = document.querySelector('.close-button');
 const pagesField = document.getElementById('pages');
+const submitBtn = document.querySelector('input[type ="submit"]');
 
 trigger.addEventListener('click', toggleModal);
 closeBtn.addEventListener('click', toggleModal);
 window.addEventListener('click', windowOnCLick);
 pagesField.addEventListener('keypress', checkPages);
+submitBtn.addEventListener('click', submit);
+cardContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('isread-btn')) {
+    myLibrary.updateIsRead.call(e.target);
+    myLibrary.addBorder.call(e.target);
+  }
+  if (e.target.classList.contains('remove-btn')) {
+    myLibrary.delete(e.target);
+  } else if (e.target.parentNode.classList.contains('remove-btn')) {
+    myLibrary.delete(e.target.parentNode);
+  }
+});
 
+//functions
 function toggleModal() {
   modalContainer.classList.toggle("show");
   clearFields();
 }
-
 
 function windowOnCLick(e) {
   if (e.target === modalContainer) {
     toggleModal();
   }
 }
-
 
 function checkPages(e) {
   const charCode = e.charCode;
@@ -145,10 +141,6 @@ function checkPages(e) {
   e.preventDefault();
   return false;
 }
-
-
-const submitBtn = document.querySelector('input[type ="submit"]');
-submitBtn.addEventListener('click', submit);
 
 function submit(e) {
   const titleInput = document.getElementById('title');
@@ -166,7 +158,7 @@ function submit(e) {
     setTimeout(function () {
       warningMsg.style.display = 'none';
     }, 2000);
-  } else if (myLibrary.some(book => book.title.toLowerCase() === title.toLowerCase() && book.author.toLowerCase() === author.toLowerCase())) {
+  } else if (myLibrary.bookshelf.some(book => book.title.toLowerCase() === title.toLowerCase() && book.author.toLowerCase() === author.toLowerCase())) {
     const existsWarningMsg = document.querySelector('.exists');
     existsWarningMsg.style.display = 'block';
 
@@ -175,13 +167,12 @@ function submit(e) {
     }, 2000);
   } else {
     toggleModal();
-    const newBook = new Book(title, author, pages, false);
-    addBookToLibrary(newBook, myLibrary);
+    const newBook = createBook(title, author, pages, false);
+    myLibrary.add(newBook);
   }
 
   e.preventDefault();
 }
-
 
 function clearFields() {
   const titleInput = document.getElementById('title');
